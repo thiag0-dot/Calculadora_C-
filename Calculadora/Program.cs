288b9769defea7bbdb61fe39b8e30460c4b701a5
thiag0-dot <thiagodeginofilho@gmail.com>
2022-03-23 21:34:26 -0300

    first commit

---
+++ Calculadora/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite outro numero:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("1 = adição");
+            Console.WriteLine("2 = subtração");
+            Console.WriteLine("3 = Multiplicação");
+            Console.WriteLine("4 = divisão");
+            Console.WriteLine("Qual operação deseja fazer?");
+            int op = Convert.ToInt32(Console.ReadLine());
+            if (op == 1)
+            {
+                double resultado = num1 + num2;
+                Console.WriteLine("O valor da operação é de:" + resultado);
+            }
+            else if (op == 2)
+            {
+                double resultado = num2 - num1;
+                Console.WriteLine("O valor da operação é de:" + resultado);
+            }
+            else if (op == 3)
+            {
+                double resultado = num1 * num2;
+                Console.WriteLine("O valor da operação é de:" + resultado);
+            }
+            else if (op == 4)
+            {
+                double resultado = num1 / num2;
+                Console.WriteLine("O valor da operação é de:" + resultado);
+            }
+            else 
+            {
+            }
+        }
+    }
+}
---
+++ Calculadora/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Calculadora")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Calculadora")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("c9145cba-97f1-4a02-bef3-5f873df4f4d8")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
